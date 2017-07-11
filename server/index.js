2ba9a8c70a03d8518e9a67f446be32ac5ad3ebad
John Crisostomo <jd@snapzio.com>
2017-07-11 10:18:56 +0800

    server setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log('Hello');
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+const app = express();
+
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on: ', port);
