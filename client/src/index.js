4920f9251d00281a90ce8aee404193b0bf8321fe
John Crisostomo <johncrisostomo@protonmail.ch>
2017-07-12 21:51:47 +0800

    implements individual route auth

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
+import Feature from './components/feature';
+import requireAuth from './components/auth/require_auth';
 import SignIn from './components/auth/signin';
 import SignOut from './components/auth/signout';
 import SignUp from './components/auth/signup';
@@ -21,8 +23,9 @@ ReactDOM.render(
         <Route path="/signin" component={SignIn} />
         <Route path="/signup" component={SignUp} />
         <Route path="/signout" component={SignOut} />
+        <Route path="/feature" component={requireAuth(Feature)} />
       </div>
     </Provider>
   </Router>,
-  document.querySelector('.container'),
+  document.querySelector('.container')
 );
