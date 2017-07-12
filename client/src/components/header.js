1b767eef7aa9f37fcf0cda2fcb622aa9cbe997e0
John Crisostomo <jd@snapzio.com>
2017-07-12 15:21:37 +0800

    implements a smarter header based on the auth status

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  renderLinks() {
+    if (this.props.authenticated) {
+      return (
+        <li className="nav-item">
+          <Link className="nav-link" to="/signout">
+            Sign Out
+          </Link>
+        </li>
+      );
+    } else {
+      return [
+        <li className="nav-item" key={1}>
+          <Link className="nav-link" to="/signin">
+            Sign in
+          </Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link className="nav-link" to="/signup">
+            Sign up
+          </Link>
+        </li>,
+      ];
+    }
+  }
+
   render() {
     return (
       <nav className="navbar navbar-light">
+        <Link to="/" className="navbar-brand">
+          Redux Auth
+        </Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item">Sign in</li>
+          {this.renderLinks()}
         </ul>
       </nav>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = state => ({
+  authenticated: state.auth.authenticated,
+});
+
+export default connect(mapStateToProps)(Header);
